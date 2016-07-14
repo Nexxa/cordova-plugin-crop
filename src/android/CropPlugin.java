a8650e18135a6f6cec91f5bc76f39c9afddd0fc0
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-07-14 14:45:00 +0200

    missing curly brace

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -123,7 +123,8 @@ public class CropPlugin extends CordovaPlugin {
 
         // Create the cache directory if it doesn't exist
         cache.mkdirs();
-
+    }
+    
     public Bundle onSaveInstanceState() {
         Bundle state = new Bundle();
 
